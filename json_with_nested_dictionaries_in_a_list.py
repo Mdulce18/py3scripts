6eb77c658f1daef012a39f5de74acec0600de044
Mdulce18 <42708203+Mdulce18@users.noreply.github.com>
2021-02-18 16:19:40 -0300

    Update json_with_nested_dictionaries_in_a_list.py

--- json_with_nested_dictionaries_in_a_list.py
+++ json_with_nested_dictionaries_in_a_list.py
@@ -1,7 +1,7 @@
 #  Go trought a list containing dicctionaries from a json
 list_of_dic = response['json']['2']['something']  # list with dictionaries
             for dictionaries in list_of_dic:
-                for key, value in dic.items():
+                for key, value in dicionaries.items():
                     value_in_string = str(value)  # use the value searched
                     if 'ABC' in value_in_string:
                         abc_string_to_list = value_in_string.split()
